0c46db5535f661a611dbb2522b93e80381e5fc33
Abhishek <109077869+abhishekkotangale@users.noreply.github.com>
2023-07-03 22:20:06 +0530

    Update hcf.cpp

--- Numbers/hcf.cpp
+++ Numbers/hcf.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+using namespace std;
 
+int main(){
+
+    int a , b;
+    cin >> a >> b;
+
+    int hcf = 1;
+
+    for (int i = 1; i <=a or i<=b ; i++)
+    {
+        if (a%i==0 and b%i==0)
+        {
+            hcf = i;
+        }
+        
+    }
+    
+    cout << hcf;
+
+    return 0;
+}
